09535e5ae15329a5dd525df3a3c15cb744797c61
f4rral <f4rral@gmail.com>
2024-12-17 21:12:42 +0300

    Композиции контейнеров

--- app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
@@ -3,45 +3,69 @@ package com.hfad.metanitjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hfad.metanitjetpackcompose.ui.theme.MetanitJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MetanitJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ListLanguages()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ListLanguages() {
+    val languages = listOf(
+        Language("Kotlin", 0xff16a085),
+        Language("Java", 0xff2980b9),
+        Language("JavaScript", 0xffe74c3c),
+        Language("Python", 0xffd35400)
     )
+
+    Column {
+        for(lang in languages) {
+            Row(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color(lang.hexColor))
+                )
+                Text(
+                    text = lang.name,
+                    fontSize = 28.sp,
+                    modifier = Modifier
+                        .padding(10.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MetanitJetpackComposeTheme {
-        Greeting("Android")
+        ListLanguages()
     }
 }
+
+data class Language(val name: String, val hexColor: Long)
