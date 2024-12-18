411cdd7364be5eb39664069bccbeab096b800477
f4rral <f4rral@gmail.com>
2024-12-18 22:00:10 +0300

    LazyVerticalGrid, LazyHorizontalGrid

--- app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -48,16 +60,24 @@ fun ListLanguages() {
         Language("Swift", 0xff273746)
     )
 
-    Column {
-        LazyRow {
-            items(languages) { ColumnLanguage(it)}
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize().weight(1f)
+        ) {
+            items(items = languages) { ColumnLanguage(it) }
         }
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(3),
+            modifier = Modifier.fillMaxSize().weight(1f)
         ) {
-            items(languages) { RowLanguage(it) }
+            items(items = languages) {
+                RowLanguage(it)
+            }
         }
     }
 }
@@ -66,8 +86,7 @@ fun ListLanguages() {
 fun ColumnLanguage(lang: Language) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
@@ -90,7 +109,6 @@ fun RowLanguage(lang: Language) {
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .padding(16.dp)
-            .fillMaxWidth()
     ) {
         Box(
             modifier = Modifier
