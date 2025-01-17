1ff49e59a4ca30f0bda85a00420dec2e2c26b99c
f4rral <f4rral@gmail.com>
2025-01-17 13:29:13 +0300

    Пример навигации

--- app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/hfad/metanitjetpackcompose/MainActivity.kt
@@ -3,119 +3,107 @@ package com.hfad.metanitjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ListLanguages()
+            Main()
         }
     }
 }
 
 @Composable
-fun ListLanguages() {
-    val languages = listOf(
-        Language("Kotlin", 0xff16a085),
-        Language("Java", 0xff2980b9),
-        Language("JavaScript", 0xffe74c3c),
-        Language("Python", 0xffd35400),
-        Language("C++", 0xffa569bd),
-        Language("C#", 0xff21618c),
-        Language("PHP", 0xff2c3e50),
-        Language("TypeScript", 0xffa93226),
-        Language("Ruby ", 0xff839192),
-        Language("Go ", 0xffd4ac0d),
-        Language("Swift", 0xff273746)
-    )
+fun Main() {
+    // Контроллер навигации - управление стеком навигации
+    val navController = rememberNavController()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-    ) {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxSize().weight(1f)
-        ) {
-            items(items = languages) { ColumnLanguage(it) }
-        }
+    Column(Modifier.padding(8.dp).fillMaxSize()) {
+        NavBar(navController = navController)
 
-        LazyHorizontalGrid(
-            rows = GridCells.Fixed(3),
-            modifier = Modifier.fillMaxSize().weight(1f)
-        ) {
-            items(items = languages) {
-                RowLanguage(it)
-            }
+        // Host навигации - это специальный компонент, который добавляется в макет пользовательского
+        // интерфейса действия и служит заполнителем для страниц, по которым будет
+        // перемещаться пользователь
+        NavHost(navController, startDestination = NavRouters.Home.route) {
+            composable(NavRouters.Home.route) { Home() }
+            composable(NavRouters.About.route) { About() }
+            composable(NavRouters.Contacts.route) { Contacts() }
         }
     }
 }
 
 @Composable
-fun ColumnLanguage(lang: Language) {
-    Column(
-        modifier = Modifier
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .size(64.dp)
-                .background(Color(lang.hexColor))
-        )
-        Text(
-            text = lang.name,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-        )
-    }
-}
-
-@Composable
-fun RowLanguage(lang: Language) {
+fun NavBar(navController: NavController) {
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(16.dp)
+        Modifier.fillMaxWidth().padding(bottom = 8.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .size(64.dp)
-                .background(Color(lang.hexColor))
+        Text("Home",
+            Modifier
+                .weight(0.33f)
+                .clickable {
+                    navController.navigate(NavRouters.Home.route)
+                },
+            fontSize = 22.sp, color = Color(0xFF6650a4)
         )
-        Text(
-            text = lang.name,
-            fontSize = 24.sp,
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
+        Text("Contacts",
+            Modifier
+                .weight(0.33f)
+                .clickable {
+                    navController.navigate(NavRouters.Contacts.route)
+                },
+            fontSize = 22.sp, color = Color(0xFF6650a4)
+        )
+        Text("About",
+            Modifier
+                .weight(0.33f)
+                .clickable {
+                    navController.navigate(NavRouters.About.route)
+                },
+            fontSize = 22.sp, color = Color(0xFF6650a4)
         )
     }
 }
 
-@Preview(showSystemUi = true)
+@Composable
+fun Home() {
+    Text("Home Page", fontSize = 30.sp)
+}
+
+@Composable
+fun Contacts() {
+    Text("Contacts Page", fontSize = 30.sp)
+}
+
+@Composable
+fun About() {
+    Text("About Page", fontSize = 30.sp)
+}
+
+sealed class NavRouters(val route: String) {
+    object Home: NavRouters("home")
+    object Contacts: NavRouters("contacts")
+    object About: NavRouters("about")
+}
+
+@Preview(showSystemUi = false, showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ListLanguages()
+    Main()
 }
-
-data class Language(val name: String, val hexColor: Long)
